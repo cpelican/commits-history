b2b0db69b49e8eea9fecd214d5c100bbe9b3cc2f
cpelican <45201987+cpelican@users.noreply.github.com>
2022-09-15 16:09:10 +0200

    chore: fix eslint config, add prettier, add luxon clsx and friends

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {render} from '@testing-library/react';
 import App from './App';
+import {Settings} from 'luxon';
+import {Hooks, UseCommitsApiType} from './hooks';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+describe('App', () => {
+    test.each<UseCommitsApiType>([
+      [[], false, ''],
+      [[], true, ''],
+      [[], true, 'End of the world error!'],
+      [[], false, 'End of the world error!'],
+      [[], false, null],
+      [[{
+        sha: '1234',
+        commit: {
+            author: {
+                date: '2022-09-14T04:45:56Z'
+            },
+            message: 'commit message'
+        },
+        author: {
+            avatar_url: 'www.avatar.com',
+            login: 'some name'
+        }
+      }], false, ''],
+    ])('renders as expected whith useCommitApi returning api values %s, isLoading %s, error %s', (apiValue, isLoading, error) => {
+      jest.spyOn(Hooks, 'useCommitsApi').mockReturnValue([apiValue, isLoading, error]);
+      Settings.now = () => new Date(2022, 12, 25).valueOf();
+      const {asFragment} = render(<App />);
+      expect(asFragment()).toMatchSnapshot();
+    });
 });
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
 import React from 'react';
 import './App.scss';
+import {CommitItem} from './components/CommitItem';
+import {APIItem, Hooks} from './hooks';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [items, isLoading, error] = Hooks.useCommitsApi();
+
+    function renderCommitItem(item: APIItem) {
+      const author = {
+        name: item.author.login,
+        imageUrl: item.author.avatar_url
+      };
+        return (
+            <CommitItem
+              key={item.sha}
+              sha={item.sha}
+              date={item.commit.author.date}
+              message={item.commit.message}
+              author={author}
+            />
+        );
+    }
+
+    function renderCommits() {
+      if ((items ?? []).length === 0) {
+        return null;
+      }
+      return <ul className='commit-list-container'>{items?.map(renderCommitItem)}</ul>;
+    }
+    const isLoadingMessage = isLoading ? <p>{isLoading}</p> : null;
+    return (
+        <div className='commit-history-container'>
+            {isLoadingMessage}
+            {error && <p>{error}</p>}
+            {renderCommits()}
+        </div>
+    );
 }
 
 export default App;
